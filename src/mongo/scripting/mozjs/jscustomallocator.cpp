b908897d24e1c0436465a01108d3a18cfb4c0118
Jason Carey <jcarey@argv.me>
2015-06-17 18:01:24 -0400

    more fixes

--- src/mongo/scripting/mozjs/jscustomallocator.cpp
+++ src/mongo/scripting/mozjs/jscustomallocator.cpp
@@ -1,10 +1,40 @@
+/*    Copyright 2015 MongoDB Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects
+ *    for all of the code used other than as permitted herein. If you modify
+ *    file(s) with this exception, you may extend this exception to your
+ *    version of the file(s), but you are not obligated to do so. If you do not
+ *    wish to do so, delete this exception statement from your version. If you
+ *    delete this exception statement from all source files in the program,
+ *    then also delete it in the license file.
+ */
+
+#include "mongo/platform/basic.h"
+
 #include "jscustomallocator.h"
 
+#include "mongo/config.h"
 #include "mongo/util/concurrency/threadlocal.h"
 
-#ifdef HAVE_MALLOC_H
+#ifdef __linux__
 #include <malloc.h>
-#elif defined(HAVE_MALLOC_SIZE)
+#elif defined(__APPLE__)
 #include <malloc/malloc.h>
 #else
 #error "need some kind of malloc usable size"
@@ -51,11 +81,11 @@ void* wrap_alloc(T&& func, size_t bytes) {
 }
 
 size_t get_current(void* ptr) {
-#if defined(HAVE_MALLOC_USABLE_SIZE)
+#if defined(__linux__)
     return malloc_usable_size(ptr);
-#elif defined(HAVE_MALLOC_SIZE)
+#elif defined(__APPLE__)
     return malloc_size(ptr);
-#elif HAVE__MSIZE
+#elif defined(_WIN32)
     return _msize(ptr);
 #else
 #error "Must have something like malloc_usable_size"
