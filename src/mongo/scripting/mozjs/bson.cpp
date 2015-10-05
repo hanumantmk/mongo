2107e433a59c0509f71c727841ee563a30e78144
Jason Carey <jcarey@argv.me>
2015-10-05 18:18:18 -0400

    some more bson improvements

--- src/mongo/scripting/mozjs/bson.cpp
+++ src/mongo/scripting/mozjs/bson.cpp
@@ -39,6 +39,7 @@
 #include "mongo/scripting/mozjs/objectwrapper.h"
 #include "mongo/scripting/mozjs/valuereader.h"
 #include "mongo/scripting/mozjs/valuewriter.h"
+#include "mongo/util/string_map.h"
 
 namespace mongo {
 namespace mozjs {
@@ -87,7 +88,7 @@ struct BSONHolder {
     bool _resolved;
     bool _readOnly;
     bool _altered;
-    std::set<std::string> _removed;
+    StringMap<bool> _removed;
 };
 
 BSONHolder* getValidHolder(JSContext* cx, JSObject* obj) {
@@ -135,7 +136,7 @@ void BSONInfo::enumerate(JSContext* cx, JS::HandleObject obj, JS::AutoIdVector& 
 
         // TODO: when we get heterogenous set lookup, switch to StringData
         // rather than involving the temporary string
-        if (holder->_removed.count(e.fieldName()))
+        if (holder->_removed.find(e.fieldName()) != holder->_removed.end())
             continue;
 
         ValueReader(cx, &val).fromStringData(e.fieldNameStringData());
@@ -156,7 +157,8 @@ void BSONInfo::setProperty(
             uasserted(ErrorCodes::BadValue, "Read only object");
         }
 
-        auto iter = holder->_removed.find(IdWrapper(cx, id).toString());
+        JSStringWrapper jsstr;
+        auto iter = holder->_removed.find(IdWrapper(cx, id).toStringData(&jsstr));
 
         if (iter != holder->_removed.end()) {
             holder->_removed.erase(iter);
@@ -178,7 +180,8 @@ void BSONInfo::delProperty(JSContext* cx, JS::HandleObject obj, JS::HandleId id,
 
         holder->_altered = true;
 
-        holder->_removed.insert(IdWrapper(cx, id).toString());
+        JSStringWrapper jsstr;
+        holder->_removed[IdWrapper(cx, id).toStringData(&jsstr)] = true;
     }
 
     *succeeded = true;
@@ -194,9 +197,11 @@ void BSONInfo::resolve(JSContext* cx, JS::HandleObject obj, JS::HandleId id, boo
     }
 
     IdWrapper idw(cx, id);
-    std::string sname = idw.toString();
+    JSStringWrapper jsstr;
 
-    if (!holder->_readOnly && holder->_removed.count(sname)) {
+    auto sname = idw.toStringData(&jsstr);
+
+    if (!holder->_readOnly && holder->_removed.find(sname) != holder->_removed.end()) {
         return;
     }
 
