18544f93563a3acd9da4eb4bd30870abbe3f61b2
Kevin Rosendahl <kevindrosendahl@gmail.com>
2019-07-18 12:24:09 -0700

    add different wasm scripts

---
+++ passthrough.js
@@ -0,0 +1,11 @@
+db.test.drop();
+db.test.insert({ foo: 1 });
+db.test.insert({ foo: 2 });
+db.test.insert({ foo: 3 });
+
+load("src/mongo/scripting/passthrough-wasm.js");
+
+c = db.test.aggregate([{ $wasm: { wasm: BinData(0, wasm) } }]);
+while (c.hasNext()) {
+    print(tojson(c.next()));
+}
--- script.js
+++
@@ -1,6 +0,0 @@
-db.test.drop();
-db.test.insert({x:1});
-
-load("src/mongo/scripting/passthrough-wasm.js");
-
-print(tojson(db.test.aggregate([{$wasm: {wasm: BinData(0, wasm)} }]).next()));
